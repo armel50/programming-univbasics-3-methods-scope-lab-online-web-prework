fa888f613dc5dd14c75102fd9a704a43be7fb9b9
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-12 22:55:07 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,8 +3,10 @@ def mario
   phrase = "It\'s-a me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
+  return status
 end
 
 def toadstool
+  status = mario
   puts status
 end
